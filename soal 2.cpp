b7e4d9bdeb4e365fd71c844718675c0839938728
abdulrahman04 <44152611+abdulrahman04@users.noreply.github.com>
2018-10-31 13:05:42 +0700

    first commit

---
+++ soal 2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{int N,X,T,Batas;
+N = 02;
+Batas = 02 + 100;
+X = 20;
+T = 02;
+while( T <= Batas)
+     { T = T + X;
+       X = X + 10;
+     }
+ cout << T;
+
+}
+
